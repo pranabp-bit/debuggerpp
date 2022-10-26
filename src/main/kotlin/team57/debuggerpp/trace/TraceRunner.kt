bb8ad23f745c59991f72ff5db810ece7eb869105
Sam Schweigel <s.schweigel@gmail.com>
2022-10-26 14:09:44 -0700

    Add incredibly hacky "Run with Tracing" executor for demo

--- src/main/kotlin/team57/debuggerpp/trace/TraceRunner.kt
+++ src/main/kotlin/team57/debuggerpp/trace/TraceRunner.kt
@@ -1,21 +1,24 @@
 package team57.debuggerpp.trace
 
+import ca.ubc.ece.resess.slicer.dynamic.slicer4j.Slicer
+import ca.ubc.ece.resess.slicer.dynamic.slicer4j.instrumenter.JavaInstrumenter
 import com.intellij.execution.ExecutionException
-import com.intellij.execution.ExecutionManager
+import com.intellij.execution.configurations.JavaParameters
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.impl.DefaultJavaProgramRunner
-import com.intellij.execution.jar.JarApplicationCommandLineState
 import com.intellij.execution.jar.JarApplicationConfiguration
-import com.intellij.execution.process.ProcessAdapter
-import com.intellij.execution.process.ProcessEvent
-import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.runners.ProcessProxyFactory
-import com.intellij.execution.runners.RunContentBuilder
-import com.intellij.execution.ui.RunContentDescriptor
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import java.util.concurrent.atomic.AtomicReference
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import java.io.BufferedInputStream
+import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+import java.util.jar.JarInputStream
+import kotlin.io.path.createTempFile
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.pathString
 
 @Suppress("UnstableApiUsage")
 class TraceRunner : DefaultJavaProgramRunner() {
@@ -29,40 +32,35 @@ class TraceRunner : DefaultJavaProgramRunner() {
     override fun canRun(executorId: String, profile: RunProfile): Boolean =
         executorId == "Trace" && profile is JarApplicationConfiguration
 
-    override fun execute(environment: ExecutionEnvironment) {
 
-        val profile = environment.runProfile
-        if (profile !is JarApplicationConfiguration) return
-        val state = environment.state
-        if (state !is JarApplicationCommandLineState) throw ExecutionException("Run with Tracing is supported on the local machine only")
-
-        val executionManager = ExecutionManager.getInstance(environment.project)
-        executionManager.startRunProfileWithPromise(environment, state) {
-            FileDocumentManager.getInstance().saveAllDocuments()
-            state.prepareTargetToCommandExecution(
-                environment, LOG, "Failed to execute tracing run configuration async"
-            ) {
-                val proxy = ProcessProxyFactory.getInstance().createCommandLineProxy(state)
-                val executionResult = state.execute(environment.executor, this)
-                val handler = executionResult.processHandler
-                if (proxy != null) {
-                    if (handler != null) {
-                        proxy.attach(handler)
-                        handler.addProcessListener(object : ProcessAdapter() {
-                            override fun processTerminated(event: ProcessEvent) {
-                                proxy.destroy()
-                                handler.removeProcessListener(this)
-                            }
-                        })
-                    }
-                }
-                val result = AtomicReference<RunContentDescriptor>()
-                ApplicationManager.getApplication().invokeAndWait {
-                    val contentBuilder = RunContentBuilder(executionResult, environment)
-                    result.set(contentBuilder.showRunContent(environment.contentToReuse))
-                }
-                result.get()
+    /*
+     * TODO: Find a way for IntelliJ to consider this a build task that does not need to be repeated if we've already
+     * instrumented this JAR before.
+     */
+    override fun patch(
+        javaParameters: JavaParameters, settings: RunnerSettings?, runProfile: RunProfile, beforeExecution: Boolean
+    ) {
+        if (runProfile !is JarApplicationConfiguration) throw ExecutionException(
+            "Run with Tracing is only supported on JAR applications"
+        )
+        super.patch(javaParameters, settings, runProfile, beforeExecution)
+        val task = object : Task.Modal(runProfile.project, "Instrumenting JAR", false) {
+            override fun run(indicator: ProgressIndicator) {
+                // TODO: All of this is temporary.  We don't want to be leaving temporary files everywhere, but it
+                //  will work for the demo.
+                val inJar = JarInputStream(BufferedInputStream(FileInputStream(runProfile.jarPath)))
+                val mainClass = inJar.manifest.mainAttributes.getValue("Main-Class")
+                val outPath = createTempFile("slicer4j-instrumented", ".jar")
+                val loggerPath = createTempFile("slicer4-logger", ".jar")
+                val loggerJar = Slicer::class.java.getResourceAsStream("/DynamicSlicingLogger.jar")
+                Files.copy(loggerJar, loggerPath, StandardCopyOption.REPLACE_EXISTING)
+                val instrumenter = JavaInstrumenter(outPath.pathString)
+                instrumenter.start("", "/tmp/slicer4j-static.log", javaParameters.jarPath, loggerPath.toString())
+                javaParameters.classPath.add(outPath.pathString)
+                javaParameters.mainClass = mainClass
+                loggerPath.deleteIfExists()
             }
         }
+        task.queue()
     }
 }
--- vendor/slicer4j/Slicer4J/build.gradle.kts
+++ vendor/slicer4j/Slicer4J/build.gradle.kts
@@ -16,10 +16,14 @@ dependencies {
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.jgrapht:jgrapht-core:1.2.0")
     implementation("commons-cli:commons-cli:1.4")
+    implementation("org.slf4j:slf4j-simple:1.7.5")
 
     constraints {
         implementation("org.soot-oss:soot:4.3.0") {
             because("soot-infoflow:2.9.0 on maven central has a broken dependency")
         }
     }
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 }
