ec3831add4d74422ad4105c8aabbf29ab00b4685
LuuO <luuo2000@outlook.com>
2022-10-26 17:40:46 -0700

    Upgrade soot-infoflow to 2.10.0

--- vendor/slicer4j/Slicer4J/build.gradle.kts
+++ vendor/slicer4j/Slicer4J/build.gradle.kts
@@ -10,19 +10,13 @@ dependencies {
     implementation(project(":vendor:slicing-core:core", "default"))
 
     implementation("guru.nidi:graphviz-java:0.18.1")
-    implementation("de.tud.sse:soot-infoflow:2.9.0")
+    implementation("de.fraunhofer.sit.sse.flowdroid:soot-infoflow:2.10.0")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.json:json:20201115")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.jgrapht:jgrapht-core:1.2.0")
     implementation("commons-cli:commons-cli:1.4")
     implementation("org.slf4j:slf4j-simple:1.7.5")
-
-    constraints {
-        implementation("org.soot-oss:soot:4.3.0") {
-            because("soot-infoflow:2.9.0 on maven central has a broken dependency")
-        }
-    }
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
