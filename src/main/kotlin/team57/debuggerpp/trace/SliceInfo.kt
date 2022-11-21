429fa7b719436da76679b6bbb3e3e86a4e09d26c
arya23065 <arya.subramanyam@alumni.ubc.ca>
2022-11-21 03:42:28 -0800

    Added empty Slicer4J DebugSessionTab on info button press

--- src/main/kotlin/team57/debuggerpp/trace/SliceInfo.kt
+++ src/main/kotlin/team57/debuggerpp/trace/SliceInfo.kt
@@ -1,17 +1,47 @@
 package team57.debuggerpp.trace
+import com.intellij.execution.ui.RunnerLayoutUi
+import com.intellij.execution.ui.layout.PlaceInGrid
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.markup.HighlighterLayer.*
+import com.intellij.openapi.editor.markup.HighlighterLayer.SELECTION
 import com.intellij.openapi.editor.markup.MarkupModel
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
+import com.intellij.ui.content.Content
+import com.intellij.xdebugger.XDebugSession
+import com.intellij.xdebugger.XDebuggerManager
 import java.awt.Color
 
 class SliceInfo: AnAction() {
-    override fun actionPerformed(event: AnActionEvent) {
+    override fun actionPerformed(e: AnActionEvent) {
+        grayOutNonSliceLines(e)
+        createSlicerInfoView(e)
+    }
+
+    fun createSlicerInfoView(event: AnActionEvent) {
+        val project: Project = event.getRequiredData(CommonDataKeys.PROJECT)
+        val editor = event.getRequiredData(CommonDataKeys.EDITOR)
+
+        val session: XDebugSession? = XDebuggerManager.getInstance(project).getCurrentSession()
+        val ui: RunnerLayoutUi = session!!.getUI()
+        ui.defaults.initTabDefaults(1000, "Slicer4J", null)
+
+        val view = SlicerInfoTab()
+        val content: Content = ui.createContent(
+                "SlicerContentId",
+                view,
+                "Slicer4J", null, null
+        )
+        content.isCloseable = false
+        ui.addContent(content, 1000, PlaceInGrid.left, false)
+//        Disposer.register(session.runContentDescriptor, view)
+    }
+
+    fun grayOutNonSliceLines(event: AnActionEvent) {
+        // TO-DO: fix always select project file - right now it chooses the editor window that is selected
         val editor: Editor = event.getRequiredData(CommonDataKeys.EDITOR)
         val project: Project = event.getRequiredData(CommonDataKeys.PROJECT)
         val document: Document = editor.getDocument()
@@ -27,6 +57,5 @@ class SliceInfo: AnAction() {
         }
 
 //        markupModel.removeAllHighlighters()
-
     }
 }
---
+++ src/main/kotlin/team57/debuggerpp/trace/SlicerInfoTab.kt
@@ -0,0 +1,14 @@
+package team57.debuggerpp.trace
+
+import com.intellij.util.ui.components.BorderLayoutPanel
+import java.awt.BorderLayout
+import javax.swing.BorderFactory
+import javax.swing.JComponent
+import javax.swing.JLabel
+
+class SlicerInfoTab: JComponent {
+
+        constructor() {
+        }
+
+}
