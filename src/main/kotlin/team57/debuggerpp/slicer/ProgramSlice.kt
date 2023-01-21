999871292109c807284ffe944b4192e5a44753a5
LuuO <luuo2000@outlook.com>
2023-01-21 01:21:41 -0800

    Extract dependencies from DynamicSlice

--- src/main/kotlin/team57/debuggerpp/slicer/ProgramSlice.kt
+++ src/main/kotlin/team57/debuggerpp/slicer/ProgramSlice.kt
@@ -2,6 +2,7 @@ package team57.debuggerpp.slicer
 
 import ca.ubc.ece.resess.slicer.dynamic.core.slicer.DynamicSlice
 import com.intellij.openapi.diagnostic.Logger
+import java.util.*
 
 class ProgramSlice(private val dynamicSlice: DynamicSlice) {
     companion object {
@@ -18,4 +19,58 @@ class ProgramSlice(private val dynamicSlice: DynamicSlice) {
         }
         return@run map
     }
+
+    val dependencies: Map<SourceLocation, Dependencies> = run {
+        val map = HashMap<SourceLocation, Dependencies>()
+        for (entry in dynamicSlice) {
+            val fromNode = entry.o1.o1
+            val toNode = entry.o2.o1
+            val fromLocation = SourceLocation(fromNode.javaSourceFile, fromNode.javaSourceLineNo)
+            val toLocation = SourceLocation(toNode.javaSourceFile, toNode.javaSourceLineNo)
+            val dependenciesFrom = map.getOrPut(fromLocation) { Dependencies() }
+            val dependenciesTo = map.getOrPut(toLocation) { Dependencies() }
+            when (val type = dynamicSlice.getEdges(entry.o1.o1.lineNo, entry.o2.o1.lineNo)) {
+                "data" -> {
+                    (dependenciesFrom.data.to as ArrayList).add(DataDependency(toLocation, entry.o2.o2.pathString))
+                    (dependenciesTo.data.from as ArrayList).add(DataDependency(fromLocation, entry.o2.o2.pathString))
+                }
+
+                "control" -> {
+                    (dependenciesFrom.control.to as ArrayList).add(ControlDependency(toLocation))
+                    (dependenciesTo.control.from as ArrayList).add(ControlDependency(fromLocation))
+                }
+
+                else -> throw IllegalStateException("Unknown dependency type $type")
+            }
+        }
+        return@run map
+    }
+
+    class SourceLocation(val clazz: String, val lineNo: Int) {
+        override fun toString() = "$clazz:$lineNo"
+
+        override fun equals(other: Any?) = (other is SourceLocation)
+                && clazz == other.clazz
+                && lineNo == other.lineNo
+
+        override fun hashCode() = Objects.hash(clazz, lineNo)
+    }
+
+    class Dependencies(
+        val data: DataDependencies = DataDependencies(),
+        val control: ControlDependencies = ControlDependencies()
+    )
+
+    class DataDependencies(val from: List<DataDependency> = ArrayList(), val to: List<DataDependency> = ArrayList())
+
+    class ControlDependencies(
+        val from: List<ControlDependency> = ArrayList(),
+        val to: List<ControlDependency> = ArrayList()
+    )
+
+    open class Dependency(val location: SourceLocation)
+
+    class ControlDependency(location: SourceLocation) : Dependency(location)
+
+    class DataDependency(location: SourceLocation, val variableName: String) : Dependency(location)
 }
