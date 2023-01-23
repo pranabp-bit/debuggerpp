eecd9b1b9e907238b3569f7afba98ad98860a3db
arya23065 <arya.subramanyam@alumni.ubc.ca>
2023-01-22 22:10:47 -0800

    Added SelectSlicingCriteriaOption to popup menu

---
+++ src/main/kotlin/team57/debuggerpp/ui/SelectSlicingCriterionOption.kt
@@ -0,0 +1,19 @@
+package team57.debuggerpp.ui
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+
+class SelectSlicingCriterionOption: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        // Selects the line the cursor is currently on, regardless of any highlighting
+        val editor = e.getData(CommonDataKeys.EDITOR)
+        val offset = editor?.caretModel?.offset
+        val document = editor?.document
+        val lineNo = offset?.let { document?.getLineNumber(it) }?.plus(1)
+        println("Selected slicing criterion line number is $lineNo")
+
+        val fileName = e.getData(CommonDataKeys.VIRTUAL_FILE)?.name
+        println("File Name is $fileName")
+    }
+}
