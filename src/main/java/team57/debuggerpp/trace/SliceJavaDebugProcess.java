85d0b73c5c5a1fb4c611d41364088a64c329fa84
Robin Lai <robinlaiyy@gmail.com>
2023-02-02 01:48:11 -0800

    add popup window when selected line is not in slice

--- src/main/java/team57/debuggerpp/trace/SliceJavaDebugProcess.java
+++ src/main/java/team57/debuggerpp/trace/SliceJavaDebugProcess.java
@@ -1,24 +1,31 @@
 package team57.debuggerpp.trace;
 
+import com.intellij.debugger.engine.DebugProcessImpl;
 import com.intellij.debugger.engine.JavaDebugProcess;
+import com.intellij.debugger.engine.evaluation.EvaluateException;
 import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ui.UIUtil;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebuggerManager;
 import com.intellij.xdebugger.XDebuggerUtil;
+import com.intellij.xdebugger.XSourcePosition;
 import com.intellij.xdebugger.breakpoints.XBreakpointManager;
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.intellij.xdebugger.frame.XSuspendContext;
-import com.intellij.xdebugger.impl.XSourcePositionImpl;
+import com.intellij.xdebugger.impl.actions.XDebuggerActions;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import team57.debuggerpp.slicer.ProgramSlice;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SliceJavaDebugProcess extends JavaDebugProcess {
     public final ProgramSlice slice;
@@ -36,18 +43,48 @@ public class SliceJavaDebugProcess extends JavaDebugProcess {
         return res;
     }
 
+    private VirtualFile getVirtualFile(XSuspendContext context){
+        VirtualFile virtualFile = context.getActiveExecutionStack().getTopFrame().getSourcePosition().getFile();
+        return virtualFile;
+    }
     @Override
     public void startStepInto(@Nullable XSuspendContext context) {
-        VirtualFile virtualFile = context.getActiveExecutionStack().getTopFrame().getSourcePosition().getFile();
-
         this.slice.getSliceLinesUnordered().forEach((a, b) -> {
             b.forEach(lineNumber -> {
-                XDebuggerUtil.getInstance().toggleLineBreakpoint(getSession().getProject(), virtualFile, lineNumber-1, true);
+                XDebuggerUtil.getInstance().toggleLineBreakpoint(getSession().getProject(), getVirtualFile(context), lineNumber-1, true);
             });
         });
         getDebuggerSession().resume();
     }
 
+    int currentPosition = 0; // TODO: update this to initial breakpoint position
+    @Override
+    public void runToPosition(@NotNull XSourcePosition position, @Nullable XSuspendContext context) {
+        this.slice.getSliceLinesUnordered().forEach((a, b) -> {
+            int realLinePosition = position.getLine() + 1;
+            XSourcePosition myPosition = position;
+            if(b.contains(realLinePosition)) {
+                currentPosition = myPosition.getLine();
+                myPosition = XDebuggerUtil.getInstance().createPosition(getVirtualFile(context), realLinePosition-1);
+                super.runToPosition(myPosition, context);
+            }
+            else{
+                Messages.showErrorDialog("The line you selected is out of the slice, please try again!", UIUtil.removeMnemonic(ActionsBundle.actionText(XDebuggerActions.RUN_TO_CURSOR)));
+//                myPosition = XDebuggerUtil.getInstance().createPosition(getVirtualFile(context), currentPosition);
+            }
+
+        });
+//
+//        try {
+//            DebugProcessImpl.ResumeCommand runToCursorCommand = getDebuggerSession().getProcess().createRunToCursorCommand(getDebuggerSession().getContextManager().getContext().getSuspendContext(), position, false);
+//                    myDebugProcess.createRunToCursorCommand(getSuspendContext(), position, ignoreBreakpoints);
+//            setSteppingThrough(runToCursorCommand.getContextThread());
+//            resumeAction(runToCursorCommand, DebuggerSession.Event.STEP);
+//        }
+//        catch (EvaluateException e) {
+//            Messages.showErrorDialog(e.getMessage(), UIUtil.removeMnemonic(ActionsBundle.actionText(XDebuggerActions.RUN_TO_CURSOR)));
+//        }
+    }
     private void addLineBreakpoint(final Project project, final String fileUrl, final int line) {
         class MyBreakpointProperties extends XBreakpointProperties<MyBreakpointProperties> {
             public String myOption;
