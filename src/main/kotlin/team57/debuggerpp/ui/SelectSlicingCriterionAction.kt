5f4a21dc7922c210c1d305720a5c96774ff0fe58
LuuO <luuo2000@outlook.com>
2023-03-07 22:51:38 -0800

    Add support for Current File "run configuration"

--- src/main/kotlin/team57/debuggerpp/ui/SelectSlicingCriterionAction.kt
+++ src/main/kotlin/team57/debuggerpp/ui/SelectSlicingCriterionAction.kt
@@ -3,17 +3,19 @@ package team57.debuggerpp.ui
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.RunManager
-import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.execution.actions.RunConfigurationsComboBoxAction
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
+import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiClass
 import com.intellij.psi.util.PsiTreeUtil
 import team57.debuggerpp.execute.DynamicSliceDebuggerExecutor
+import team57.debuggerpp.execute.RunCurrentFile
 import team57.debuggerpp.util.SourceLocation
 
 class SelectSlicingCriterionAction : AnAction() {
@@ -26,7 +28,6 @@ class SelectSlicingCriterionAction : AnAction() {
         // Selects the line the cursor is currently on, regardless of any highlighting
         val editor = e.getData(CommonDataKeys.EDITOR)!!
         val psiFile = e.getData(CommonDataKeys.PSI_FILE)!!
-        val project = editor.project!!
         val offset = editor.caretModel.offset
         val document = editor.document
         val lineNo = document.getLineNumber(offset) + 1
@@ -41,18 +42,26 @@ class SelectSlicingCriterionAction : AnAction() {
         val fileName = e.getData(CommonDataKeys.VIRTUAL_FILE)?.name
         println("File Name is $fileName")
         LOG.info("Class Name is ${clazz.qualifiedName}")
-        startSliceDebugger(project, SourceLocation(clazz.qualifiedName!!, lineNo))
+        startSliceDebugger(e, SourceLocation(clazz.qualifiedName!!, lineNo))
     }
 
-    private fun startSliceDebugger(project: Project, criteria: SourceLocation) {
-        // TODO: notify the user
-        val configuration: RunnerAndConfigurationSettings = RunManager.getInstance(project).selectedConfiguration
-            ?: throw IllegalStateException("no selectedConfiguration")
-
+    private fun startSliceDebugger(e: AnActionEvent, criteria: SourceLocation) {
+        val project = e.project!!
+        var selectedConfig = RunManager.getInstance(project).selectedConfiguration
+        if (selectedConfig == null && RunConfigurationsComboBoxAction.hasRunCurrentFileItem(project)) {
+            val psiFile = e.getData(CommonDataKeys.PSI_FILE)!!
+            for (runConfig in RunCurrentFile.getRunConfigsForCurrentFile(psiFile, true)) {
+                runConfig?.let { selectedConfig = runConfig }
+                break
+            }
+        }
+        if (selectedConfig == null) {
+            throw IllegalStateException("no selected configuration and no current file config")
+        }
         val builder = ExecutionEnvironmentBuilder.create(
             project,
             DynamicSliceDebuggerExecutor.instance!!,
-            configuration.configuration
+            selectedConfig!!.configuration
         )
         val env = builder.build()
         env.putUserData(SLICING_CRITERIA_KEY, criteria)
